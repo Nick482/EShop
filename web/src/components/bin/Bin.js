862dbe2bf9348fceda855e50088bcc285ddcfd90
Nick482 <kolya.bandura@gmail.com>
2015-08-12 12:45:46 +0300

    binFile

---
+++ web/src/components/bin/Bin.js
@@ -0,0 +1,43 @@
+define(function(require){
+
+    var Backbone = require('backbone'),
+        _ = require('underscore'),
+        $ = require('jquery'),
+        binTemplate = require("text!./template/binTemplate.htm");
+
+
+    var Selected = Backbone.View.extend({
+
+        el: function(){
+            var self = this;
+            return	_.template("<div id='bin'></div>")({
+                options: self.options
+            });
+        },
+
+        events: {
+            "click #bin" : "openBin"
+        },
+
+        constructor: function(data){
+            this.options = data;
+            this.initialize();
+        },
+
+        initialize: function () {
+            this.setElement(this.el());
+        },
+
+        openBin: function(event){
+            this.trigger("openBin", this, event)
+        },
+
+
+
+        render: function(){
+            this.$el.html(_.template(binTemplate)({options: this.options}));
+            $('#container').append(this.$el);
+        }
+    });
+    return Selected;
+});
