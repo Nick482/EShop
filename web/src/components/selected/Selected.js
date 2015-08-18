bc8c93a565cd7f6397c5b898ea2ec78df00d302b
Nick482 <kolya.bandura@gmail.com>
2015-08-18 12:10:35 +0300

    Selected

--- web/src/components/selected/Selected.js
+++ web/src/components/selected/Selected.js
@@ -3,38 +3,48 @@ define(function(require){
     var Backbone = require('backbone'),
         _ = require('underscore'),
         $ = require('jquery'),
-        selectedTemplate = require("text!./template/selectedTemplate.htm");
+        selectedTemplate = require("text!./template/selectedTemplate.htm"),
+        itemModel = require('components/main/model/itemModel');
 
 
     var Selected = Backbone.View.extend({
 
         el: function(){
             var self = this;
-            return	_.template("<div id='selectedItem'></div>")({
-                options: self.options
+            return	_.template("<div id='selectedItem' code='<%= options.code %>'></div>")({
+                options: self.model.toJSON()
             });
         },
 
         events: {
-        "click #back" : "back"
+            "click #back" : "back",
+            "click #addToCart": "add"
         },
 
         constructor: function(data){
-            this.options = data;
+            this.model = new itemModel(data || {});
             this.initialize();
         },
 
         initialize: function () {
             this.setElement(this.el());
-            this.render();
         },
 
         back: function(event){
           this.trigger("backToSearch", this, event)
         },
 
+        add: function(){
+            this.trigger("addToCart", this, this.model)
+        },
+
+        setModelData: function(data){
+            this.model.set(data);
+            this.render();
+        },
+
         render: function(){
-            this.$el.html(_.template(selectedTemplate)({options: this.options}));
+            this.$el.html(_.template(selectedTemplate)({options: this.model.toJSON()}));
             $('#container').append(this.$el);
         }
     });
