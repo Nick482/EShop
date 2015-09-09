fa47e7658cc19dea5b8b4b2e30ba22921d72c707
Nick482 <kolya.bandura@gmail.com>
2015-09-09 18:44:19 +0300

    Refactored Selected Item View

--- web/src/components/selected/SelectedItemView.js
+++ web/src/components/selected/SelectedItemView.js
@@ -4,38 +4,44 @@ define(function(require){
         _ = require('underscore'),
         $ = require('jquery'),
         selectedTemplate = require("text!./template/selectedTemplate.htm"),
-        itemModel = require('components/main/model/itemModel');
+		eventBus = require('components/eventBus'),
+        ItemModel = require('components/main/model/ItemModel');
 
 
     var Selected = Backbone.View.extend({
 
         el: function(){
             var self = this;
-            return	_.template("<div id='selectedItem' code='<%= options.code %>'></div>")({
+            return	_.template("<div id='selectedItem' style='display:none' code='<%= options.code %>'></div>")({
                 options: self.model.toJSON()
             });
         },
 
         events: {
-            "click #back" : "back",
-            "click #addToCart": "add"
+            "click #back" : "_backToSearchView",
+            "click #addToCart": "_addToCart"
         },
 
         constructor: function(data){
-            this.model = new itemModel(data || {});
-            this.initialize();
+            this.model = new ItemModel(data || {});
+            
+			Backbone.View.apply(this, arguments);
+        },
+		
+        _backToSearchView: function(){
+			this.trigger("backToSearch");
         },
 
-        initialize: function () {
-            this.setElement(this.el());
-        },
-
-        back: function(event){
-          this.trigger("backToSearch", this, event)
-        },
-
-        add: function(){
-            this.trigger("addToCart", this, this.model)
+		toggle: function(){
+			this.$el.toggle("slide", { direction: "right" }, 1000);
+		},
+		
+		hide: function(){
+			this.$el.hide();
+		},
+		
+        _addToCart: function(){
+            eventBus.trigger("addToCart", this, this.model)
         },
 
         setModelData: function(data){
@@ -45,7 +51,7 @@ define(function(require){
 
         render: function(){
             this.$el.html(_.template(selectedTemplate)({options: this.model.toJSON()}));
-            $('#container').append(this.$el);
+            return this;
         }
     });
     return Selected;
