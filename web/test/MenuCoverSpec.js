fcc51adb7c1c53ef3d36d3a1564405a8fa9b6354
Nick482 <kolya.bandura@gmail.com>
2015-09-29 20:06:45 +0300

    MenuCoverSpec

---
+++ web/test/MenuCoverSpec.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Nick on 9/29/2015.
+ */
+define(function(require) {
+    var MenuCover = require('components/menu/MenuCover');
+
+    describe("MenuCover", function () {
+        it("should return an object, i guess", function () {
+            expect(false).toBe(false)
+        })
+    });
+});
