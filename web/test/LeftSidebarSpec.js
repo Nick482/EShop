067f5895738a2ee6159061ffbf490f076462647c
Nick482 <kolya.bandura@gmail.com>
2015-09-29 20:06:29 +0300

    LeftSidebarSpec

---
+++ web/test/LeftSidebarSpec.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Nick on 9/29/2015.
+ */
+define(function(require) {
+    var LeftSidebar = require('components/sidebar/LeftSideBar');
+
+    describe("LeftSidebar", function () {
+        it("should return an object, i guess", function () {
+            expect(false).toBe(false)
+        })
+    });
+});
