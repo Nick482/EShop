3b4e8ccb4a6b2ad6bbedeef8fb18ed4b5b9e5375
Nick482 <kolya.bandura@gmail.com>
2015-09-29 19:44:43 +0300

    Spec for LogoView

---
+++ web/test/LogoViewSpec.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Nick on 9/25/2015.
+ */
+define(function(require) {
+    var Logo = require('components/logo/LogoView');
+
+    describe("Logo", function () {
+        it("should return an object, i guess", function () {
+            expect(false).toBe(false)
+        })
+    });
+});
