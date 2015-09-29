fc6062d0a05ac6e2edd72fedd2b9508ecf9dba9c
Nick482 <kolya.bandura@gmail.com>
2015-09-29 20:07:13 +0300

    SearchSpec

---
+++ web/test/SearchSpec.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Nick on 9/29/2015.
+ */
+define(function(require) {
+    var Search = require('components/search/SearchView');
+
+    describe("Search", function () {
+        it("should return an object, i guess", function () {
+            expect(false).toBe(false)
+        })
+    });
+});
