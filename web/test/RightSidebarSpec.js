fcc84961e64d2fa383000284be9d31c3c4b8d8b7
Nick482 <kolya.bandura@gmail.com>
2015-09-29 20:07:06 +0300

    RightSidebarSpec

---
+++ web/test/RightSidebarSpec.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Nick on 9/29/2015.
+ */
+define(function(require) {
+    var RightSidebar = require('components/sidebar/RightSidebar');
+
+    describe("RightSidebar", function () {
+        it("should return an object, i guess", function () {
+            expect(false).toBe(false)
+        })
+    });
+});
