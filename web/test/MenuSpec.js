ab0b0c74b82aeb5c505f3fe3d607e1b88ba02567
Nick482 <kolya.bandura@gmail.com>
2015-09-29 20:06:51 +0300

    MenuSpec

---
+++ web/test/MenuSpec.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Nick on 9/29/2015.
+ */
+define(function(require) {
+    var Menu = require('components/menu/MainMenu');
+
+    describe("Menu", function () {
+        it("should return an object, i guess", function () {
+            expect(false).toBe(false)
+        })
+    });
+});
