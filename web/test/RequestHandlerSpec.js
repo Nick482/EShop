b115da041ca3d4ab7065711ffa8444ecbc450842
Nick482 <kolya.bandura@gmail.com>
2015-09-29 20:06:59 +0300

    RequestHandlerSpec

---
+++ web/test/RequestHandlerSpec.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Nick on 9/29/2015.
+ */
+define(function(require) {
+    var RequestHandler = require('components/helpers/RequestHandler');
+
+    describe("RequestHandler", function () {
+        it("should return an object, i guess", function () {
+            expect(false).toBe(false)
+        })
+    });
+});
