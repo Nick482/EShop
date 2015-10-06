b088bda7e034fe9e0f5090dde9f2f7a6d9415f66
Nick482 <kolya.bandura@gmail.com>
2015-10-06 13:10:35 +0300

    AddSpec.js

--- web/test/AddSpec.js
+++ web/test/AddSpec.js
@@ -5,8 +5,15 @@ define(function(require) {
     var Add = require('components/add/Add');
 
     describe("Add", function () {
+
+        var add;
+
+        beforeEach(function() {
+            add = new Add();
+        });
+
         it("should return an object, i guess", function () {
-            expect(false).toBe(false)
+            expect(add).toBeDefined();
         })
     });
 });
