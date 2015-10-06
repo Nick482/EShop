8d4c1a9eb66d3046016f8863779f106917bc8951
Nick482 <kolya.bandura@gmail.com>
2015-10-06 13:10:44 +0300

    ContentViewSpec

--- web/test/ContentViewSpec.js
+++ web/test/ContentViewSpec.js
@@ -2,11 +2,21 @@
  * Created by Nick on 9/29/2015.
  */
 define(function(require) {
-    var ContentView = require('components/contentView/ContentView');
+    var ContentView = require('components/contentView/ContentView'),
+
+        sinon = require('lib/sinon/lib/sinon');
 
     describe("ContentView", function () {
+
+        var contentView;
+
+        beforeEach(
+            sinon.stub(contentView),
+            contentView = new ContentView()
+        );
+
         it("should return an object, i guess", function () {
-            expect(false).toBe(false)
+            expect(contentView).toBeDefined();
         })
     });
 });
